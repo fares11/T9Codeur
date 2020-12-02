470a319c6744d04640097ebcf65b05e1c5be868d
fares <labidimedfares@gmail.com>
2020-12-02 11:30:45 +0100

    new files t9Encoder

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Declare Dictionary to put all existing choice of user on T9 keybord
+      Dictionary<char, int> digits = new Dictionary<char, int>();
+      // message written
+      Console.Write("Veuillez entrer votre message: ");
+      string message = Console.ReadLine();
+      
+      //Fill up dictionarywith digits
+      Dictionary<char, int> digitResults = Utils.addDigits(digits);
+
+      //Call the function to decode message
+      StringBuilder encodedNumber = T9.decodeMessage(message, digits);
+
+      //launch console to show result
+      Console.WriteLine(encodedNumber);
+      Console.ReadKey();
+    }
+  }
+}
---
+++ T9.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+  public class T9
+  {
+    /// <summary>
+    /// Decode entered message
+    /// </summary>
+    /// <param name="msg"> written text</param>
+    /// <param name="digits">Dictionary that contains choices in T9 Keyboard</param>
+    /// <returns>String that contains decoded message</returns>
+    public static StringBuilder decodeMessage(String msg, Dictionary<char, int> digits)
+    {
+      StringBuilder encodedNumber = new StringBuilder();
+      if (!String.IsNullOrEmpty(msg))
+      {
+        foreach (char value in msg)
+        {
+          foreach (KeyValuePair<char, int> k in digits)
+          {
+            if (value == k.Key)
+            {
+              encodedNumber.Append(k.Value.ToString());
+              break;
+            }
+          }
+        }
+      }
+      return encodedNumber;
+    }
+  }
+}
