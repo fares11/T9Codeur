bd2acc662f6c2f3fa52f7e7587f876f908aced70
fares <labidimedfares@gmail.com>
2020-12-10 20:47:40 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace ConsoleApp1
 {
@@ -8,17 +6,12 @@ namespace ConsoleApp1
   {
     static void Main(string[] args)
     {
-      // Declare Dictionary to put all existing choice of user on T9 keybord
-      Dictionary<char, int> digits = new Dictionary<char, int>();
       // message written
       Console.Write("Veuillez entrer votre message: ");
       string message = Console.ReadLine();
-      
-      //Fill up dictionarywith digits
-      Dictionary<char, int> digitResults = Utils.addDigits(digits);
 
       //Call the function to decode message
-      StringBuilder encodedNumber = T9.decodeMessage(message, digits);
+      string encodedNumber = T9.encodeMessage(message);
 
       //launch console to show result
       Console.WriteLine(encodedNumber);
--- T9.cs
+++ T9.cs
@@ -1,37 +1,80 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Globalization;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace ConsoleApp1
 {
   public class T9
   {
     /// <summary>
-    /// Decode entered message
+    /// Normalizes a string and removes diacritics.
     /// </summary>
     /// <param name="msg"> written text</param>
-    /// <param name="digits">Dictionary that contains choices in T9 Keyboard</param>
-    /// <returns>String that contains decoded message</returns>
-    public static StringBuilder decodeMessage(String msg, Dictionary<char, int> digits)
+    public static string removeDiacritics(String msg)
     {
+      //Normalizes unicode characters
+      string normalizedText = msg.Normalize(NormalizationForm.FormD);
       StringBuilder encodedNumber = new StringBuilder();
-      if (!String.IsNullOrEmpty(msg))
-      {
         foreach (char value in msg)
         {
-          foreach (KeyValuePair<char, int> k in digits)
+          //Obtains unicode character category
+          UnicodeCategory unicodeChar = CharUnicodeInfo.GetUnicodeCategory(value);
+          if (unicodeChar != UnicodeCategory.NonSpacingMark)
           {
-            if (value == k.Key)
-            {
-              encodedNumber.Append(k.Value.ToString());
-              break;
-            }
-          }
+            encodedNumber.Append(value);
+          }          
         }
-      }
-      return encodedNumber;
+      return encodedNumber.ToString().Normalize(NormalizationForm.FormC);
+    }
+
+    /// <summary>
+    /// Encodes a string to the T9 format.
+    /// </summary>
+    /// <param name="msg">written text</param>
+    /// <returns></returns>
+    public static string encodeMessage(string msg)
+    {
+      string result = removeDiacritics(msg);
+      result = Regex.Replace(result, "[2-9]", string.Empty);
+
+      result = Regex.Replace(result, "[a]", "2");
+      result = Regex.Replace(result, "[b]", "22");
+      result = Regex.Replace(result, "[c]", "222");
+
+      result = Regex.Replace(result, "[d]", "3");
+      result = Regex.Replace(result, "[e]", "33");
+      result = Regex.Replace(result, "[f]", "333");
+
+      result = Regex.Replace(result, "[g]", "4");
+      result = Regex.Replace(result, "[h]", "44");
+      result = Regex.Replace(result, "[i]", "444");
+
+      result = Regex.Replace(result, "[j]", "5");
+      result = Regex.Replace(result, "[k]", "55");
+      result = Regex.Replace(result, "[l]", "555");
+
+      result = Regex.Replace(result, "[m]", "6");
+      result = Regex.Replace(result, "[n]", "66");
+      result = Regex.Replace(result, "[o]", "666");
+
+      result = Regex.Replace(result, "[p]", "7");
+      result = Regex.Replace(result, "[q]", "77");
+      result = Regex.Replace(result, "[r]", "777");
+      result = Regex.Replace(result, "[s]", "7777");
+
+      result = Regex.Replace(result, "[t]", "8");
+      result = Regex.Replace(result, "[u]", "88");
+      result = Regex.Replace(result, "[v]", "888");
+
+      result = Regex.Replace(result, "[w]", "9");
+      result = Regex.Replace(result, "[x]", "99");
+      result = Regex.Replace(result, "[y]", "999");
+      result = Regex.Replace(result, "[z]", "9999");
+
+      result = Regex.Replace(result, "[^2-9]", " ");
+
+      return result;
     }
   }
 }
